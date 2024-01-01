0acbe5235e7f0393bbdf35948bf46bdbb1575c3e
gingmzmzx <2417481092@qq.com>
2024-01-01 16:22:28 +0800

    Change

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import json
 
 history = list()
 while True:
-    inputStr = input(">>> ")
+    inputStr = input("User > ")
     historyTmp = copy.deepcopy(history)
     history.append({"role":"user","content":inputStr})
     result = requests.post("https://cursor.sh/api/chat/stream", data=json.dumps({"prompt":json.dumps({"history":history}), "history":historyTmp})).content.decode("utf-8")
     history.append({"role":"system","content":result})
-    print(result)
+    print("CursorAI >", result)
